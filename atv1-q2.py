a63a5034a4c53f60e10552909bc687512f9b4528
Ihago Nunes <ihago2nunes@gmail.com>
2026-02-25 21:36:45 -0300

    Create atv1-q2.py

---
+++ atv1-q2.py
@@ -0,0 +1,24 @@
+# (1,5) Um vetor A contém n inteiros retirados do intervalo [0, 4n], com repetições permitidas.
+# Implemente um programa em Python contendo uma função para determinar o valor inteiro k
+# que ocorre com mais frequência.
+# Requisitos 
+# 
+# a) O programa deve receber como entrada um array contendo n inteiros retirados do intervalo
+# [0, 4n]. Os valores deverão ser informados via terminal e não podem estar fora do intervalo
+# especificado. O valor de n também deverá ser informado via teclado.
+# 
+# b) O programa deve imprimir no terminal o elemento com frequência máxima e sua frequência 
+# associada.
+# 
+# c) Caso todos os valores apresentam a mesma frequência imprimir a mensagem "Todos os 
+# valores apresentam a mesma frequência".
+# 
+# d) O programa não pode usar recursividade.
+
+
+n = int(input(f"Digite a quantidade de inteiros para o vetor"))
+a = [1]*3
+#for i in range(n):
+    
+ 
+print(a)
